85b7271fdcd383e810c4491f2d8e299d8e7f6150
Kaya-1412 <kaya1412s@gmail.com>
2025-10-17 17:57:05 +0200

    Worker sketch

---
+++ PodzielWyplatyWorker.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Soneta.Business;
+using Soneta.Business.App;
+using Soneta.Core;
+using Soneta.Ksiega;
+using Soneta.Place;
+using Soneta.Towary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Soneta.Kasa;
+using System.Collections.Immutable;
+using Soneta.Kasa.Extensions;
+using Soneta.Types;
+using Google.Apis.Util;
+
+namespace A1.PodzielWyplaty
+{
+    public class PodzielWyplatyWorker
+    {
+
+        public List<Soneta.Place.Wyplata> wyp {  get; set; }
+
+        public PodzielWyplatyWorker(ListaPlac lp) 
+        {
+            this.wyp = lp.Wyplaty.ToList();
+        }
+        public void Podziel()
+        {
+            foreach (Soneta.Place.Wyplata w in wyp)
+            {
+                ElemSlownika projekt;
+               
+                Dictionary<ElemSlownika, decimal> projekty = new Dictionary<ElemSlownika, decimal>();
+                
+                
+                foreach(WypElement el in w.Elementy)
+                {
+                    projekt = el.Definicja.Features["Projekty"] as ElemSlownika;
+                   
+                    if(!projekty.TryAdd(projekt, el.Netto))
+                    {
+                        projekty[projekt] += el.Netto;
+                    }
+                }
+                foreach (var key in projekty)
+                    if (projekty[key.Key].Equals(decimal.Zero))
+                        projekty.Remove(key.Key);
+
+                
+                KasaModule km = KasaModule.GetInstance(w.Session);
+                using (var t = w.Session.Logout(true))
+                {
+                    w.ListaPlac.Features["Podzielona"] = true;
+                    foreach(Platnosc p in w.Platnosci)
+                    {
+                        decimal pozostalaWartoscPlatnosci = p.Kwota.Value;
+                        
+                        for (int i = 0; i < projekty.Keys.Count; i++)
+                        {
+
+                            
+                            var key = projekty.Keys.ElementAt(i);
+                            
+                        
+                            if (projekty[key] == decimal.Zero)
+                                continue;
+                            p.Opis = $"Wypłata za: {key.ToString()}";
+                            if (pozostalaWartoscPlatnosci < projekty[key])
+                            {
+                                
+                               
+                                projekty[key] -= pozostalaWartoscPlatnosci;
+                                pozostalaWartoscPlatnosci = decimal.Zero;
+                                break;
+                            }
+                            else if (pozostalaWartoscPlatnosci == projekty[key])
+                            {
+                               
+                                projekty[key] -= pozostalaWartoscPlatnosci;
+                                pozostalaWartoscPlatnosci = decimal.Zero;
+                                break;
+                            }
+                            else if (pozostalaWartoscPlatnosci > projekty[key])
+                            {
+                               
+                                p.Kwota = projekty[key];
+                                pozostalaWartoscPlatnosci -= projekty[key];
+                                projekty[key] = decimal.Zero;
+                                
+                                while (pozostalaWartoscPlatnosci > decimal.Zero)
+                                {
+                                    i++;
+                                    key = projekty.Keys.ElementAt(i);
+                                    if (projekty[key] == decimal.Zero)
+                                        break;
+                                    Zobowiazanie zobowiazanie = new Zobowiazanie(w);
+                                    km.Platnosci.AddRow(zobowiazanie);
+                                    zobowiazanie.Podmiot = p.Podmiot;
+                                    zobowiazanie.SposobZaplaty = p.SposobZaplaty;
+                                    zobowiazanie.Rachunek = p.Rachunek;
+                                    zobowiazanie.EwidencjaSP = p.EwidencjaSP;
+                                    zobowiazanie.Termin = p.Termin;
+                                    zobowiazanie.Opis = $"Wypłata za: {key.ToString()}";
+                                    if (projekty[key] < pozostalaWartoscPlatnosci)
+                                    {
+                                        zobowiazanie.Kwota = projekty[key];
+                                        pozostalaWartoscPlatnosci -= projekty[key];
+                                        projekty[key] = decimal.Zero;
+
+                                    }
+                                    else if (projekty[key] > pozostalaWartoscPlatnosci)
+                                    {
+                                        zobowiazanie.Kwota = pozostalaWartoscPlatnosci;
+                                        projekty[key] -= pozostalaWartoscPlatnosci;
+                                        pozostalaWartoscPlatnosci = decimal.Zero;
+                                    }
+                                    else
+                                        break;
+                                }
+                            }   
+                        }
+                    }
+                    
+                    t.Commit();
+                }
+            }
+        }
+
+    }
+}
---
+++ PodzielWyplatyWorkerUI.cs
@@ -0,0 +1,60 @@
+﻿using AltOne.KSM.UI;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Org.BouncyCastle.Asn1.X509;
+using Soneta.Business;
+using Soneta.Business.UI;
+using Soneta.Core.Extensions;
+using Soneta.Kadry;
+using Soneta.KadryPlace;
+using Soneta.Core;
+using System;
+using A1.PodzielWyplaty;
+using Soneta.Place;
+
+[assembly: Worker(typeof(PodzielWyplatyWorkerUI), typeof(ListaPlac))]
+
+namespace AltOne.KSM.UI
+{
+    public class PodzielWyplatyWorkerUI
+    {
+        /*
+        [Context]
+        public PodzielWyplatyWorkerParams @params
+        {
+            get;
+            set;
+        }
+        */
+        [Context]
+        public ListaPlac lp {get; set;}
+
+        [Action("Podziel wypłaty", Mode = ActionMode.SingleSession | ActionMode.ConfirmSave | ActionMode.Progress,
+            Target = ActionTarget.Menu | ActionTarget.ToolbarWithText | ActionTarget.LocalMenu)]
+        
+        public object PodzielWyplaty(){
+
+            if (lp == null){
+                throw new ArgumentNullException();
+            }
+            return new MessageBoxInformation() {
+                Text = "Czy podzielić wypłaty?",
+                YesHandler = () => {
+                   
+                    PodzielWyplatyWorker pww = new(lp);
+                    pww.Podziel();
+                   
+                    return "Pomyślnie podzielono wypłaty";
+                },
+                NoHandler = () => "Operacja przerwana"
+            };
+        }
+    }
+
+    /*public class PodzielWyplatyWorkerParams : ContextBase
+    {
+        public PodzielWyplatyWorkerParams(Context context) : base(context)
+        {
+        }
+    }
+    */
+}
